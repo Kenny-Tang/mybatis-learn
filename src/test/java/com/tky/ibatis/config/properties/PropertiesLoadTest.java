04dcc7c09401363378b12fec9687792d72760f55
kenny-tang <kenny-tang@hotmail.com>
2021-02-08 14:38:04 +0800

    MyBatis 源码解读（二）自定义变量的加载

--- src/test/java/com/tky/ibatis/config/properties/PropertiesLoadTest.java
+++ src/test/java/com/tky/ibatis/config/properties/PropertiesLoadTest.java
@@ -1,22 +1,32 @@
 package com.tky.ibatis.config.properties;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Properties;
 
 import org.junit.Test;
 
 import com.tky.ibatis.BaseTest;
 
+/**
+ * <a href="https://blog.csdn.net/TangKenny/article/details/113649366">MyBatis
+ * 源码解读（二）自定义变量的加载</a>
+ * 
+ * @author Kenny
+ *
+ */
 public class PropertiesLoadTest extends BaseTest {
 
 	@Override
 	public String getResource() {
-		return "com/tky/environments/mybatis-config.xml";
+		return "com/tky/perperties/mybatis-config.xml";
 	}
 
 	@Test
 	public void testProperties() {
 		Properties properties = sqlSessionFactory.getConfiguration().getVariables();
 		logger.info(properties.toString());
+		assertEquals("kenny", properties.get("system.username"));
 	}
 
 }
